11bd7a632c89222bf320c2129059c8a3fefe0fc1
Yakimory <yastrebdev@gmail.com>
2024-03-20 18:20:33 +0700

    create webpack

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import "../styles/style.scss";
+
+const hi: string = 'hi'
+
+console.log(hi)
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // fallback to style-loader in development
+                    // process.env.NODE_ENV !== "production"
+                    //     ? "style-loader"
+                    //     : MiniCssExtractPlugin.loader,
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: 'body',
+        }),
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+            chunkFilename: "[id].css",
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+    },
+};
